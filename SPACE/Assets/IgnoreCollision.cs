22342b7124d670ee11eb5826f3afbf586fc69065
EDU-DOMAIN\hbova <hbova@mail.pima.edu>
2017-08-31 20:00:38 -0700

    Added more assets

--- SPACE/Assets/IgnoreCollision.cs
+++ SPACE/Assets/IgnoreCollision.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour {
+public class IgnoreCollision : MonoBehaviour {
+
+    public Collider2D ball;
+
+    public Collider2D wall;
 
 	// Use this for initialization
 	void Start () {
-		
+        Physics2D.IgnoreCollision(ball, wall);
 	}
 	
 	// Update is called once per frame
---
+++ SPACE/Assets/P1Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1Controller : MonoBehaviour {
+
+    public float speed = 5;
+    public float jumpHeight = 10;
+
+    public Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime,0f),ForceMode2D.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space))
+            if (player.position.y < 1 )
+                player.AddForce(new Vector2(0f,jumpHeight),ForceMode2D.Impulse);
+	}
+}
